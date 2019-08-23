65f619a882a10accb31367cdff187d54a5328571
RADHIKA <bajajrids@gmail.com>
2019-08-23 22:53:24 +0530

    Initial commit

---
+++ app/src/main/java/com/airit/test/Activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.airit.test.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.airit.test.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
